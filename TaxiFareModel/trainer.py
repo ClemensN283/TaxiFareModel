848ca119b148b726129ddbb9b07347d019c33e84
Clemens Neurauter <clemens.neurauter@outlook.com>
2021-11-16 20:54:07 +0100

    trainer modifications

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -8,6 +8,10 @@ from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from TaxiFareModel.utils import compute_rmse
+from memoized_property import memoized_property
+import mlflow
+from mlflow.tracking import MlflowClient
+
 
 class Trainer():
     def __init__(self, X_train, y_train, X_test, y_test):
@@ -71,3 +75,31 @@ if __name__ == "__main__":
     # evaluate
     rmse = trainer.evaluate(X_test, y_test)
     print(rmse)
+
+########
+
+    MLFLOW_URI = "https://mlflow.lewagon.co/"
+    EXPERIMENT_NAME = "[BR] [RJ] [Clemens] TaxiFaireModel + 1"
+
+    @memoized_property
+    def mlflow_client(self):
+        mlflow.set_tracking_uri(MLFLOW_URI)
+        return MlflowClient()
+
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(self.experiment_name)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(
+                self.experiment_name).experiment_id
+
+    @memoized_property
+    def mlflow_run(self):
+        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
